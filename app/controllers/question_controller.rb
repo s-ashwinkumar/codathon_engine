7c72d3ce916e5b3e985d903d00fe745d76f574da
Dwaraka Nath <dtsdwarak@hotmail.com>
2014-07-07 16:05:45 +0530

    Modified View

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,14 +1,14 @@
 class QuestionController < ApplicationController
   
   #CAS Authentication Docs : https://github.com/rubycas/rubycas-client#using-rubycas-client-in-rails-controllers
-  # before_filter CASClient::Frameworks::Rails::Filter
-  before_filter :check_loggedin
+  before_filter CASClient::Frameworks::Rails::Filter
+  # before_filter :check_loggedin
 
-  def check_loggedin
-  	if !(session[:cas_user])
-  		redirect_to '/'
-  	end
-  end
+  # def check_loggedin
+  # 	if !(session[:cas_user])
+  # 		redirect_to '/'
+  # 	end
+  # end
 
   def index
   end
