d25414712c6ea9d49df4523baf82ee2c4b2cf053
s-ashwinkumar <s.ashwinkumar2490@gmail.com>
2014-07-08 17:49:53 -0400

    dashboard calcualtions

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,8 @@
 class DashboardsController < ApplicationController
 	def index
+		month = params[:month] || Time.now.month
+		year = params[:year] || Time.now.year
+		@date = DateTime.new(year,month)
+		@data = User.dashboard_data(month,year)
 	end	
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,3 +1,4 @@
 class Participant < User
   # attr_accessible :title, :body
+  
 end
