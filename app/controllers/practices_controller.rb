a9c5b9d5c8b396d00961edd7696095f2c1f6c544
hameedfiaz <s.ashwinkumar2490@gmail.com>
2015-06-25 12:35:35 +0530

    revamp part 1

--- app/controllers/practices_controller.rb
+++
@@ -1,6 +0,0 @@
-class PracticesController < ApplicationController
-	layout "participant"
-	def index
-		@submissions = Submission.live_feed
-	end
-end
--- db/migrate/20150120122727_submission_belongs_to_question_test_case.rb
+++
@@ -1,9 +0,0 @@
-class SubmissionBelongsToQuestionTestCase < ActiveRecord::Migration
-  def up
-  	rename_column :submissions, :question_id, :question_test_case_id
-  end
-
-  def down
-  	rename_column :submissions, :question_test_case_id, :question_id
-  end
-end
