53ac85ff794bb98b53682bd6049d49a76b6d64cc
hameedfiaz <s.ashwinkumar2490@gmail.com>
2015-06-26 19:07:24 +0530

    challenge page for both

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -7,10 +7,11 @@ class ParticipantsController < ApplicationController
   def subscribe
     #method name is misleading. This toggles subscription.
     subscription = Subscription.where("user_id = ? and challenge_id = ?",current_user.id,params[:challenge_id])
-    flash[:success] = if subscription.present? && subscription.destroy_all
-      "Unsubscribed from Challenge #{params[:challenge_id]} successfully"
+    if subscription.present? && subscription.destroy_all
+      flash[:success] =  "Unsubscribed from Challenge #{params[:challenge_id]} successfully"
     else
-      "Subscribed to Challenge #{params[:challenge_id]} successfully"
+      Subscription.create({user_id: current_user.id, challenge_id: params[:challenge_id]})
+      flash[:success] = "Subscribed to Challenge #{params[:challenge_id]} successfully"
     end
     redirect_to send(get_home_path)
   end
